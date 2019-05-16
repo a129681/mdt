b07fca684d273690dc77840c856f7528e32ade1d
huyf <2389853653@qq.com>
2019-05-16 21:57:03 +0800

    update:add dropdown

---
+++ components/dropdown/index.tsx
@@ -0,0 +1,125 @@
+import React, { Component } from 'react'
+import './style.scss'
+export interface IProps {
+  /** change 回调 */
+  onChange?: Function
+  /** 下拉选框内容 */
+  overlay?: React.ReactNode
+  /** 显示下拉框 */
+  visible?: boolean
+  /** 是否显示尖角 */
+  arrow?: boolean
+}
+export interface IState {
+  collapse: boolean
+}
+export default class Select extends Component<IProps> {
+  state = {
+    collapse: false,
+    visible: false
+  }
+
+  static defaultProps = {
+    arrow: false
+  }
+
+  _container: any
+  _panelRef: any
+  closePanel = () => {
+    this.setState({
+      collapse: false
+    })
+  }
+
+  openPanel = () => {
+    this.setState({
+      collapse: true
+    })
+  }
+
+  togglePanel = () => {
+    const { collapse } = this.state
+    const { onChange } = this.props
+    this.setState(
+      {
+        collapse: !collapse
+      },
+      () => {
+        onChange && onChange(this.state.collapse)
+      }
+    )
+  }
+
+  clickHandler = event => {
+    const { onChange } = this.props
+    if (this._container.contains(event.target)) {
+      return
+    }
+    this.closePanel()
+    onChange && onChange(false)
+  }
+
+  componentDidUpdate (){
+    this.panelAlignCenter()
+  }
+
+  componentDidMount () {
+    document.addEventListener(
+      'click',
+      this.clickHandler
+    )
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener(
+      'click',
+      this.clickHandler
+    )
+  }
+
+  panelAlignCenter = () => {
+    const panelDom = this._panelRef
+    const containerDom = this._container
+    if (panelDom) {
+      const panelWidth = panelDom.offsetWidth
+      const containerWidth = containerDom.offsetWidth
+      const leftValue = containerWidth - panelWidth
+      panelDom.style.transform = 'translateX(' + (leftValue/2)+'px)'
+      // panelDom.style.background='red'
+      console.log(leftValue)
+    }
+  }
+
+  renderPanel = () => {
+    const { overlay, arrow } = this.props
+    return (
+      <div
+        className={`mdt-dropdown-panel ${
+          arrow ? 'mdt-dropdown-arrow' : ''
+        }`}
+        ref={(ref)=>{this._panelRef = ref}}
+      >
+        {overlay}
+      </div>
+    )
+  }
+  render () {
+    const { collapse } = this.state
+    const { visible } = this.props
+    const show = collapse || visible
+    return (
+      <div
+        className="mdt-dropdown"
+        ref={ref => (this._container = ref)}
+      >
+        <div
+          className="mdt-dropdown-target"
+          onClick={this.togglePanel}
+        >
+          {this.props.children}
+        </div>
+        {show ? this.renderPanel() : null}
+      </div>
+    )
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import { render } from 'react-dom'
-import App from './app'
 import './index.scss'
 interface IAppProps {}
 interface IAppState {}
@@ -9,7 +8,7 @@ class Index extends PureComponent < IAppProps, IAppState > {
     public render() {
         return (
             <div>
-       	<App/>
+   
       </div>
         )
     }
